53da9f5e77963e75346e5f8d113d920b5f4ca53a
Yury Izotov <bestlowry@gmail.com>
2017-04-13 03:55:02 +0500

    add fill database and some checks

--- lib/database_installer.rb
+++ lib/database_installer.rb
@@ -2,9 +2,12 @@ require "sqlite3"
 require_relative "bash_parser"
 
 class DatabaseInstaller
-  def self.create_database #создает саму БД и ее структуру
+  
+  def self.db
     db = SQLite3::Database.new "bashim.sqlite3"
+  end
 
+  def self.create_database #создает саму БД и ее структуру    
     rows = db.execute <<-SQL
       CREATE TABLE "posts" (
           "number" INTEGER PRIMARY KEY NOT NULL,
@@ -17,6 +20,14 @@ class DatabaseInstaller
 
   def self.fill_database_from_bash #заполняет БД данными с http://bash.im
     parser = BashParser.new
+    parser.posts.each do |post|
+      begin
+        db.execute("INSERT INTO posts (number, ratio, datetime, text) 
+            VALUES (?, ?, ?, ?)", [post.number, post.ratio.nil? ? 'NULL' : post.ratio, post.date, post.text])  
+      rescue SQLite3::ConstraintException #если БД уже создана и там есть записи, но уникальность не дает вставить повторяющуюся - пропускаем дубль
+        next  
+      end      
+    end
   end 
   
   
